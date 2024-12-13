fcfef34727880748cc61a36aabedcc62c4e63faa
Rahma1604 <rahmamostafa041@gmail.com>
2024-12-13 18:05:59 +0200

    in center

--- src/UIwindows/PlayGame.java
+++ src/UIwindows/PlayGame.java
@@ -2,19 +2,20 @@ package UIwindows;
 
 import UIwindows.menu.startOrExit;
 
+import javax.media.opengl.GLCanvas;
 import javax.swing.JFrame;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
+import java.awt.*;
 
 public class PlayGame {
-
     public static void main(String[] args) {
-
         JFrame gameWindow = new JFrame("Pacman Game");
         gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameWindow.setSize(800, 600);
+        gameWindow.setLocationRelativeTo(null);//علشان تظهر في نص الشاشه
         startOrExit.showMenu(gameWindow);
         gameWindow.setVisible(true);
     }
