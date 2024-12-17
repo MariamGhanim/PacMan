9b7d0058c1c512265f1f0cbbc022d125aa76399f
Alaa6122004 <alaanagah5555@gmail.com>
2024-12-17 02:18:47 -0800

    handle eating strawberry

--- src/UIwindows/menu/singleplayer/SingleLevel1Listener.java
+++ src/UIwindows/menu/singleplayer/SingleLevel1Listener.java
@@ -38,7 +38,6 @@ public class SingleLevel1Listener extends AnimListener implements KeyListener , 
     int index1 = 0,index2=0;
     int x = 40,y = 540,x2=555,y2=60;
     ArrayList<Eating> eat = new ArrayList<Eating>();
-    ArrayList<Eating> strawberry = new ArrayList<Eating>();
     ArrayList<Ghost> ghost = new ArrayList<>();
     Pacman pacman1 = new Pacman(x,y,index1);
     Pacman pacman2 = new Pacman(x2,y2,index2);
@@ -202,7 +201,7 @@ public class SingleLevel1Listener extends AnimListener implements KeyListener , 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < column; j++) {
                 if(i % 2 == 0 && j % 3 ==0) {
-                    if(map[i][j] == 1 && i == j) strawberry.add(new Eating(j,i,5));
+                    if(map[i][j] == 1 && i == j) add.add(new Eating(j,i,5));
                     else if (map[i][j] == 1) {
                         eat.add(new Eating(j, i,6));
                     }
@@ -212,9 +211,7 @@ public class SingleLevel1Listener extends AnimListener implements KeyListener , 
     }
     public void DrawFood(GL gl){
         for(Eating e : eat)
-            DrawSprite(e.ConvertX(),e.ConvertY(),e.getIndex(),1.0f);
-        for(Eating s: strawberry)
-            DrawSprite(s.ConvertX(),s.ConvertY(),s.getIndex(),0.6f);
+            DrawSprite(e.ConvertX(),e.ConvertY(),e.getIndex(),0.6f);
     }
     public void handleEat(){
         for (int i = 0; i < eat.size(); i++) {
